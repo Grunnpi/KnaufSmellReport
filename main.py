aeccb0ea5f8e1c14c901100d27df0419a6ba3ddf
Grunnpi <Mdpgithub1976+1978=4>
2024-05-20 10:55:23 +0200

    nouveau site

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import requests as req
 import argparse
 from datetime import datetime
+import json
+from urllib.parse import quote_plus
 
-
-log = 'https://odometric-synergy.com/api/controller/ObservatoryRiversideController/logToObservatory'
-pwd = 'https://odometric-synergy.com/api/controller/AccessController/checkPassword'
-logpwd = 'https://odometric-synergy.com/api/controller/ObservatoryRiversideController/logToObservatoryWithPassword'
-createDataURL = "https://odometric-synergy.com/api/controller/ObservatoryDataController/createData"
+log = 'https://api.app-synergy.tech/user/login'
+createDataURL = "https://api.app-synergy.tech/report"
 
 # partie principale
 if __name__ == "__main__":
   parser=argparse.ArgumentParser(description='Ecole Direct extact process')
 
   # Ecole Directe cred
-  parser.add_argument('--observatory_id', help='Obs ID', type=str, required=True)
-  parser.add_argument('--riverside_id', help='Riverside ID', type=str, required=True)
-
   parser.add_argument('--username', help='User name', type=str, required=True)
   parser.add_argument('--password', help='Pwd', type=str, required=True)
 
@@ -34,76 +30,46 @@ if __name__ == "__main__":
   headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
     ,'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
-    ,'Host' : 'odometric-synergy.com'
-    ,'Origin' : 'https://odometric-synergy.com'
-    ,'Referer' : 'https://odometric-synergy.com/observatoire/knauf-insulation/knauf-insulation/obsLogin'
-    ,'X-Requested-With' : 'XMLHttpRequest'
+    ,'Origin' : 'https://www.app-synergy.tech'
+    ,'Referer' : 'https://www.app-synergy.tech/'
   }
 
-  loginResult = session.post(log, headers=headers,data="observatory_id="+str(args.observatory_id)+"&username="+str(args.username))
+  #  loginPayload = "email=pierre.grunnagel%40gmail.com&password=Mdpknauf%2B24&rememberMe=true"
+  loginPayload = "email=" + quote_plus(str(args.username)) + "&password=" + quote_plus(str(args.password)) + "&rememberMe=true"
+  print(loginPayload)
+  loginResult = session.post(log, headers=headers,data=loginPayload)
   print(loginResult.text)
-  # print(session.cookies)
+  loginPayload = json.loads(loginResult.text)
 
-  headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-    ,'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
-    ,'Host' : 'odometric-synergy.com'
-    ,'Origin' : 'https://odometric-synergy.com'
-    ,'Referer' : 'https://odometric-synergy.com/observatoire/knauf-insulation/knauf-insulation/password'
-    ,'X-Requested-With' : 'XMLHttpRequest'
-  }
+  token = loginPayload["token"]["value"]
+  user_id = loginPayload["user"]["_id"]
+  observatory = loginPayload["user"]["observatoryAffiliation"]["observatory"]["_id"]
+  #print(observatory)
+  #print("user_id")
+  #print(observatory)
+  #print("user_id")
 
-  checkPasswordResult = session.post(pwd, headers=headers,data="password="+str(args.password))
-  print(checkPasswordResult.text)
-  # print(session.cookies)
-
-  headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-    ,'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
-    ,'Host' : 'odometric-synergy.com'
-    ,'Origin' : 'https://odometric-synergy.com'
-    ,'Referer' : 'https://odometric-synergy.com/observatoire/knauf-insulation/knauf-insulation/password'
-    ,'X-Requested-With' : 'XMLHttpRequest'
-  }
-  checkWithPasswordResult = session.post(logpwd, headers=headers,data="observatory_id="+str(args.observatory_id)+"&riverside_id="+str(args.riverside_id)+"&password="+str(args.password))
-  print(checkWithPasswordResult.text)
+  #print(loginPayload["token"]["value"])
 
   now = datetime.now()
-  print(str(now)[:10])
+  sToday = str(now)[:10]
 
-  # print(session.cookies)
-  payload = 'observatory_id=' + str(args.observatory_id)
-  payload += '&riverside_id=' + str(args.riverside_id)
-  payload += '&is_unavailable=false'
-  payload += "&date=" + str(now)[:10] + "+" + str(args.heure) + "%3A00"
-  payload += '&is_sick=false'
-  payload += '&is_odor=true'
-  payload += '&odor_id=8b55b5c9-caa5-4019-8dd1-9edd931ea0e6' # odeur chimique
+  iHeure = int(args.heure)
+  iHeure = iHeure - 2
+  sHeure = str(iHeure)
+  if ( len(sHeure) == 1 ):
+    sHeure = "0" + sHeure
 
-  if ( args.force == "fort"):
-    payload += '&inconvenience_id=6b36a8bc-97c4-4e12-97e4-676df425d630'
-    payload += '&intensity_id=9379a96f-0e26-425e-ad7c-6704e86a3f33'
-  else:
-    payload += '&inconvenience_id=956be207-c5ac-4d4f-a952-a80835dda4ed'
-    payload += '&intensity_id=faf7c7e2-b7ce-4269-9043-99f43c75b6e2'
-
-
-  payload += '&inconvenience_value='
-  payload += '&comment='
-  payload += '&from_date='
-  payload += '&to_date='
-  payload += '&is_noise=false'
-  payload += '&noise_id='
-
-
+  payload = "contact%5BcountryPhoneCode%5D=FR&contact%5BphoneNumber%5D=&report%5Bdate%5D=" + sToday + "T" + sHeure + "%3A00%3A00.015Z&report%5Bemission%5D=6572c9e8e60b709f489f9b1c&report%5BemissionType%5D=odor&report%5BintensityLevel%5D=6&report%5BdiscomfortLevel%5D=5&report%5BnothingToReport%5D=false&_id=" + observatory + "&vigieId=" + user_id
   headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'
-    ,'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
-    ,'Host' : 'odometric-synergy.com'
-    ,'Origin' : 'https://odometric-synergy.com'
-    ,'Referer' : 'https://odometric-synergy.com/observatoire/knauf-insulation/knauf-insulation/obsObservation'
+    ,'Content-Type' : 'application/x-www-form-urlencoded'
+    ,'Origin' : 'https://www.app-synergy.tech'
+    ,'Referer' : 'https://www.app-synergy.tech/'
     ,'X-Requested-With' : 'XMLHttpRequest'
+    ,'Authorization' : token
   }
+
   print(payload)
   createDataResult = session.post(createDataURL, headers=headers,data=payload)
   print(createDataResult.text)
